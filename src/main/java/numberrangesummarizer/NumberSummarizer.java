f03ea14980f6b7bdd8380b26d5b6dc94629e6dda
hiranya <hnaicker@student.wethinkcode.co.za>
2022-06-20 21:08:11 +0200

    refactor: test feat pom

--- src/main/java/numberrangesummarizer/NumberSummarizer.java
+++ src/main/java/numberrangesummarizer/NumberSummarizer.java
@@ -17,60 +17,49 @@ public class NumberSummarizer implements NumberRangeSummarizer{
     }
 
     @Override
-    public Collection<Integer> collect(String input) {
-
-        Collection<Integer> collectionOfInts = null;
-        try{
-            collectionOfInts = Stream.of(input.split(","))
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList());
-        } catch (NumberFormatException e){
-            throw new NumberFormatException("Invalid input \""+ input +"\". Integers required.");
-
-        } catch (NullPointerException e){
-            throw new NumberFormatException("Invalid input \"" + input + "\". Integers required.");
-
-        } catch (Exception e){
-            throw e;
-        }
-
-        return collectionOfInts;
+    public Collection<Integer> collect(String input) throws NumberFormatException, NullPointerException {
+        return Stream.of(input.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     @Override
     public String summarizeCollection(Collection<Integer> input) {
+        List<List<Integer>> arraysCollected = new ArrayList<>();
         ArrayList<Integer> sortedInput = new ArrayList<>(input);
         Collections.sort(sortedInput);
 
-        List<List<Integer>> arraysCollected = new ArrayList<>();
+        // placeholder list of current consecutive values
         List<Integer> temp = new ArrayList<>();
         temp.add(sortedInput.get(0));
 
         for (int i = 0; i < sortedInput.size() - 1; i++) {
-            if (sortedInput.get(i + 1) != sortedInput.get(i) + 1) {
+            int currentValue = sortedInput.get(i);
+            int nextValue = sortedInput.get(i + 1);
+
+            //check for consecutive values
+            if (nextValue != currentValue + 1) {
                 arraysCollected.add(temp);
                 temp = new ArrayList<>();
             }
             temp.add(sortedInput.get(i + 1));
         }
 
+        // add last list
         arraysCollected.add(temp);
 
-        List<String> summarised =new ArrayList<>();
+        List<String> summarised = new ArrayList<>();
 
-        for(List item:arraysCollected) {
-            if(item.size()>1) {
-                Integer firstElement = (Integer) item.get(0);
-                Integer lastElement = (Integer) item.get(item.size()-1);
-                String result = firstElement + "-" + lastElement;
-                summarised.add(result);
-
-            }else {
+        for(List<Integer> item : arraysCollected) {
+            if(item.size() > 1) {
+                Integer firstElement = item.get(0);
+                Integer lastElement = item.get(item.size() - 1);
+                summarised.add(firstElement + "-" + lastElement);
+            } else {
                 summarised.add(item.get(0).toString());
             }
-            }
+        }
 
-
-        return String.join(",", summarised);
+        return String.join(", ", summarised);
     }
 }
--- src/test/java/numberrangesummarizer/NumberSummarizerTest.java
+++ src/test/java/numberrangesummarizer/NumberSummarizerTest.java
@@ -13,8 +13,6 @@ class NumberSummarizerTest {
     String orderedList = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
     List<Integer> orderedNumbers = Arrays.asList(1,3,6,7,8,12,13,14,15,21,22,23,24,31);
     String unorderedList = "22,23,24,31,1,3,6,7,8,12,13,14,15,21";
-    List<Integer> unorderedNumbers = Arrays.asList(22,23,24,31,1,3,6,7,8,12,13,14,15,21);
-
 
     @Test
     void testCollectStringAndConvertToIntListOrderedTest() {
@@ -24,31 +22,21 @@ class NumberSummarizerTest {
     }
 
     @Test
-    void testCollectStringAndConvertToListOfIntsOnUnorderedSeqTest() {
-        NumberSummarizer summariser = new NumberSummarizer(unorderedList);
-        Collection<Integer> numberList = summariser.collect(unorderedList);
-        assertEquals(unorderedNumbers,numberList);
-    }
-    @Test
     void testCollectOnInvalidDataTest() {
         String input = "string";
         NumberSummarizer summariser = new NumberSummarizer(input);
-        Exception exception = assertThrows(NumberFormatException.class, () -> {
+        assertThrows(NumberFormatException.class, () -> {
             summariser.collect(input);
         });
-        assertEquals( "Invalid input \"" +input + "\". Integers required.", exception.getMessage());
-
     }
 
     @Test
     void testCollectOnNullTest() {
         String input = null;
         NumberSummarizer summariser = new NumberSummarizer(input);
-                Exception exception = assertThrows(NumberFormatException.class, () -> {
+        assertThrows(NullPointerException.class, () -> {
             summariser.collect(input);
         });
-        assertEquals( "Invalid input \"" +input + "\". Integers required.", exception.getMessage());
-
     }
 
     @Test
@@ -56,7 +44,7 @@ class NumberSummarizerTest {
         NumberSummarizer summariser = new NumberSummarizer();
         Collection<Integer> numberList = summariser.collect(orderedList);
         String result = summariser.summarizeCollection(numberList);
-        assertEquals("1,3,6-8,12-15,21-24,31",result);
+        assertEquals("1, 3, 6-8, 12-15, 21-24, 31",result);
     }
 
     @Test
@@ -64,7 +52,7 @@ class NumberSummarizerTest {
         NumberSummarizer summariser = new NumberSummarizer();
         Collection<Integer> numberList = summariser.collect(unorderedList);
         String result = summariser.summarizeCollection(numberList);
-        assertEquals("1,3,6-8,12-15,21-24,31",result);
+        assertEquals("1, 3, 6-8, 12-15, 21-24, 31",result);
     }
 
 }
